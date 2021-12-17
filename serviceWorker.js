cceb0ced96c07d9bf8d57999540cc9af79727f4e
Santiago <73190790+santic93@users.noreply.github.com>
2021-12-17 14:36:07 -0300

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,58 @@
+const CACHE_ELEMENTS = [
+  "./",
+  "https://unpkg.com/react@17/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js",
+  "https://unpkg.com/@babel/standalone/babel.min.js",
+  "./components/Contador.js",
+  "./style.css",
+];
+
+const CACHE_NAME = "v1_cache_contador_react";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache
+        .addAll(CACHE_ELEMENTS)
+        .then(() => {
+          self.skipWaiting();
+        })
+        .catch(console.log);
+    })
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cachesNames) => {
+        return Promise.all(
+          cachesNames.map((cacheName) => {
+            return (
+              cacheWhitelist.indexOf(cacheName) === -1 &&
+              caches.delete(cacheName)
+            );
+          })
+        );
+      })
+      .then(() => {
+        self.clients.claim();
+      })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if(res){
+        return res;
+      }
+      return fetch(e.request)
+    })
+  );
+});
+
+
